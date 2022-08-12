85caa671225f0f4618afe51d14c22a896e97f08f
Cologler <skyoflw@gmail.com>
2022-08-12 14:14:52 +0800

    support progress_callback

--- xlgcid.py
+++ xlgcid.py
@@ -9,10 +9,13 @@ from typing import *
 import hashlib
 import os
 
-def get_gcid_digest(fp, fp_size: int):
+def get_gcid_digest(fp, fp_size: int, *, progress_callback=None):
     '''
     Calc GCID from `fp`.
     '''
+
+    if progress_callback is None:
+        progress_callback = lambda _1, _2: None
 
     # modified from https://binux.blog/2012/03/hash_algorithm_of_xunlei/
 
@@ -24,23 +27,26 @@ def get_gcid_digest(fp, fp_size: int):
 
     buf = bytearray(piece_size)  # Reusable buffer to reduce allocations.
     buf_view = memoryview(buf)
+    read_size_sum = 0
     while read_size := fp.readinto(buf):
-        fp_size -= read_size
-        if fp_size < 0:
+        read_size_sum += read_size
+        if read_size_sum > fp_size:
             raise ValueError('visit unexpected data.')
         h.update(hashlib.sha1(buf_view[:read_size]).digest())
+        progress_callback(read_size_sum, fp_size)
 
-    if fp_size != 0:
+    if fp_size != read_size_sum:
         raise ValueError('stream end too early.')
 
     return h.digest()
 
-def get_file_gcid_digest(path: str):
+def get_file_gcid_digest(path: str, *, progress_callback=None):
     '''
     Calc GCID from `path`.
     '''
     with open(path, 'rb') as fp:
-        return get_gcid_digest(fp, os.path.getsize(path))
+        return get_gcid_digest(fp, os.path.getsize(path),
+            progress_callback=progress_callback)
 
 __all__ = [
     'get_gcid_digest',
